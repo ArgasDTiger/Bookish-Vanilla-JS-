6d383b51d90b41b29fb5a045943216e01e9445b0
ArgasDTiger <bodia02981@gmail.com>
2024-02-20 20:59:40 +0200

    Added filters to the store

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -1,7 +1,22 @@
-var dropdowns = document.getElementsByClassName("btn btn-link d-inline-block");
+let dropdowns = document.getElementsByClassName("btn btn-link d-inline-block");
+
+function closeDropdowns() {
+    for (let j = 0; j < dropdowns.length; j++) {
+        if (dropdowns[j].getAttribute('aria-expanded') === 'true') {
+            dropdowns[j].click();
+        }
+    }
+}
+
+document.addEventListener('click', function(event) {
+    if (!event.target.matches('.btn.btn-link.d-inline-block')) {
+        closeDropdowns();
+    }
+});
 
 for (let i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener('click', function() {
+    dropdowns[i].addEventListener('click', function(event) {
+        event.stopPropagation();
         for (let j = 0; j < dropdowns.length; j++) {
             if (j !== i && dropdowns[j].getAttribute('aria-expanded') === 'true') {
                 dropdowns[j].click();
--- src/assets/scripts/store.js
+++ src/assets/scripts/store.js
@@ -1,91 +1,94 @@
+async function fetchData(dataType) {
+    try {
+        const response = await fetch('./books_db.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        const resultSet = new Set([]);
+        for (let book of data.books) {
+            if (dataType === 'authors') {
+                resultSet.add(book.author);
+            } else if (dataType === 'genres') {
+                for (let genre of book.genres) {
+                    resultSet.add(genre);
+                }
+            }
+        }
+        return resultSet;
+    } catch (error) {
+        console.error(`Error fetching ${dataType}:`, error);
+        return [];
+    }
+}
+
 async function main() {
-    await addAuthorsToStore();
-    await addGenresToStore();
+    await addDataToStore('authors');
+    await addDataToStore('genres');
     await fillStore();
 }
 
 main().catch(error =>
     console.error('Error in main function:', error));
 
+
 const storeProducts = document.getElementById('storeProducts');
-async function getAuthors() {
+
+async function addDataToStore(dataType) {
     try {
-        const response = await fetch('./books_db.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        const authors = new Set([]);
-        for (let book of data.books) {
-            authors.add(book.author);
-        }
-        return authors;
-    } catch (error) {
-        console.error('Error fetching authors:', error);
-        return [];
-    }
-}
+        const data = await fetchData(dataType);
+        const dataArray = [...data];
+        dataArray.sort();
 
-async function getGenres() {
-    try {
-        const response = await fetch('./books_db.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        const genres = new Set([]);
-        for (let book of data.books) {
-            for (let genre of book.genres) {
-                genres.add(genre);
-            }
-        }
-        return genres;
-    } catch (error) {
-        console.error('Error fetching authors:', error);
-        return [];
-    }
-}
+        const dataList = document.getElementById(dataType);
 
-async function addAuthorsToStore() {
-    try {
-        const authorsSet = await getAuthors();
-        const authorsArray = [...authorsSet];
-        authorsArray.sort();
-
-        const authorsList = document.getElementById('authors');
-
-        for (let author of authorsArray) {
+        for (let item of dataArray) {
             const li = document.createElement('li');
             li.classList.add('dropdown-item');
-            li.innerHTML = author;
-            authorsList.appendChild(li);
+            li.innerHTML = item;
+
+            li.onclick = function() {
+                event.stopPropagation();
+                if (dataType === 'authors') {
+                    selectAuthor(this);
+                } else if (dataType === 'genres') {
+                    selectGenre(this);
+                }
+                searchByGenresAndAuthor();
+            };
+
+            dataList.appendChild(li);
         }
     } catch (error) {
-        console.error('Error adding authors to the store list:', error);
+        console.error(`Error adding ${dataType} to the store list:`, error);
     }
 }
 
-
-async function addGenresToStore() {
-    try {
-        const genresSet = await getGenres();
-        const genresArray = [...genresSet];
-        genresArray.sort();
-
-        const genresList = document.getElementById('genres');
-
-        for (let genre of genresArray) {
-            const li = document.createElement('li');
-            li.classList.add('dropdown-item');
-            console.log(genre);
-            li.innerHTML = genre;
-            genresList.appendChild(li);
-        }
-    } catch (error) {
-        console.error('Error adding authors to the store list:', error);
-    }
+function addBookHTML(book) {
+    return `
+        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 ms-3 me-3">
+            <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="assets/images/books/${book.image}" alt="${book.name}">
+                    <p class="rating-text">${book.rating.toFixed(1)}/10</p>
+                </div>    
+                <div class="card-body">
+                    <div class="d-flex flex-column justify-content-center align-items-center">
+                        <p class="card-name">${book.name}</p>
+                        <p class="card-price">$${book.price.toFixed(2)}</p>
+                    </div>
+                    <div class="btn-group w-100">
+                        <button class="btn btn-outline-primary">
+                            <i class="bi bi-info-lg"></i>
+                        </button>
+                        <button class="btn btn-outline-primary">
+                            <i class="bi bi-cart-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
 }
-
 
 function toggleChevron(element) {
     const span = element.getElementsByTagName('span')[0];
@@ -98,41 +101,24 @@ function toggleChevron(element) {
     }
 }
 
+async function fetchBooks() {
+    const response = await fetch('./books_db.json');
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+}
+
 async function fillStore() {
     try {
-        const response = await fetch('./books_db.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
+        const data = await fetchBooks();
         storeProducts.innerHTML = "";
 
         const resultsFound = document.getElementById('resultsFound');
         resultsFound.textContent = `Found ${data.books.length} results`;
 
         for (const book of data.books) {
-            const bookToAdd = `
-                <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 ms-3 me-3">
-                    <div class="card">
-                        <div class="card-img-container">
-                            <img class="card-img" src="assets/images/books/${book.image}" alt="${book.name}">
-                        </div>    
-                        <div class="card-body">
-                            <div class="d-flex flex-column justify-content-center align-items-center">
-                                <p class="card-name">${book.name}</p>
-                                <p class="card-price">$${book.price}</p>
-                            </div>
-                            <div class="btn-group w-100">
-                                <button class="btn btn-outline-primary">
-                                    <i class="bi bi-info-lg"></i>
-                                </button>
-                                <button class="btn btn-outline-primary">
-                                    <i class="bi bi-cart-plus"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+            const bookToAdd = addBookHTML(book);
             storeProducts.insertAdjacentHTML('beforeend', bookToAdd);
         }
     } catch (error) {
@@ -140,45 +126,127 @@ async function fillStore() {
     }
 }
 
-async function onSearchBook() {
+function selectGenre(element) {
+    let genres = document.querySelectorAll('#genres .dropdown-item');
+    genres = Array.from(genres);
+
+    if (element.textContent === 'All') {
+        genres.forEach(function(genre) {
+            if (genre.textContent === 'All') {
+                genre.classList.add('active');
+            } else {
+                genre.classList.remove('active');
+            }
+        });
+    } else {
+        element.classList.toggle('active');
+        genres.forEach(function(genre) {
+            if (genre.textContent === 'All') {
+                genre.classList.remove('active');
+            }
+        });
+    }
+    if (!genres.find(genre => genre.classList.contains('active'))) {
+        genres[0].classList.add('active');
+    }
+}
+
+function selectAuthor(element) {
+    let authors = document.querySelectorAll('#authors .dropdown-item');
+    authors = Array.from(authors);
+
+    if (element.textContent === 'All') {
+        authors.forEach(function(author) {
+            if (author.textContent === 'All') {
+                author.classList.add('active');
+            } else {
+                author.classList.remove('active');
+            }
+        });
+    } else {
+        element.classList.toggle('active');
+        authors.forEach(function(author) {
+            if (author.textContent === 'All') {
+                author.classList.remove('active');
+            }
+        });
+    }
+    if (!authors.find(author => author.classList.contains('active'))) {
+        authors[0].classList.add('active');
+    }
+}
+
+function selectSort(element) {
+    let sorts = document.querySelectorAll('#sortKinds .dropdown-item');
+    sorts = Array.from(sorts);
+
+    let selectedSort = sorts.find(sort => sort.textContent === element.textContent);
+
+    sorts.forEach(sort => sort.classList.remove('active'));
+
+    if (selectedSort) {
+        selectedSort.classList.add('active');
+    }
+
+    return selectedSort;
+}
+
+
+function getSelectedGenres() {
+    let activeGenres = document.querySelectorAll('#genres .dropdown-item.active');
+
+    return Array.from(activeGenres).map(function (genre) {
+        return genre.textContent;
+    });
+}
+
+function getSelectedAuthors() {
+    let activeAuthors = document.querySelectorAll('#authors .dropdown-item.active');
+
+    return Array.from(activeAuthors).map(function (author) {
+        return author.textContent;
+    });
+}
+
+async function searchByGenresAndAuthor() {
     try {
-        const response = await fetch('./books_db.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
+        const data = await fetchBooks();
 
         const search = document.getElementById('searchBook');
+        const selectedGenres = getSelectedGenres();
+        const selectedAuthors = getSelectedAuthors();
 
-        const books = data.books.filter(b => b.name.toLowerCase().includes(search.value.toLowerCase()));
+
+        let books = data.books.filter(book =>
+            book.name.toLowerCase().includes(search.value.trim().toLowerCase()) &&
+            (selectedGenres.includes('All') || selectedGenres.some(genre => book.genres.includes(genre))) &&
+            (selectedAuthors.includes('All') || selectedAuthors.some(author => book.author === author))
+        );
+
+        let sorts = document.querySelectorAll('#sortKinds .dropdown-item');
+        sorts = Array.from(sorts);
+        const sort = sorts.find(sort => sort.classList.contains('active'));
+        switch (sort.textContent) {
+            case 'Popularity':
+                books.sort((a, b) => b.rating - a.rating);
+                break;
+            case 'Price (ascending)':
+                books.sort((a, b) => a.price - b.price);
+                break;
+            case 'Price (descending)':
+                books.sort((a, b) => b.price - a.price);
+                break;
+            default:
+                break;
+        }
+
         storeProducts.innerHTML = "";
 
         const resultsFound = document.getElementById('resultsFound');
         resultsFound.textContent = `Found ${books.length} results`;
 
         for (const book of books) {
-            const bookToAdd = `
-                <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 ms-3 me-3">
-                    <div class="card">
-                        <div class="card-img-container">
-                            <img class="card-img" src="assets/images/books/${book.image}" alt="${book.name}">
-                        </div>    
-                        <div class="card-body">
-                            <div class="d-flex flex-column justify-content-center align-items-center">
-                                <p class="card-name">${book.name}</p>
-                                <p class="card-price">$${book.price}</p>
-                            </div>
-                            <div class="btn-group w-100">
-                                <button class="btn btn-outline-primary">
-                                    <i class="bi bi-info-lg"></i>
-                                </button>
-                                <button class="btn btn-outline-primary">
-                                    <i class="bi bi-cart-plus"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+            const bookToAdd = addBookHTML(book);
             storeProducts.insertAdjacentHTML('beforeend', bookToAdd);
         }
     } catch (error) {
